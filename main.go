31f2325520af66d5f4c7a5359b884212eb5ee014
lang <jingyifsy@gmail.com>
2024-12-16 00:01:01 +0800

    init

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"sync"
 )
 
-// 通过通信来共享内存，而不是通过共享内存来通信
-func main() {
+// 通过共享内存来通信
+func main1() {
 	num := 0
 	var mu sync.Mutex
 	var wg sync.WaitGroup
@@ -23,4 +23,62 @@ func main() {
 
 	wg.Wait()
 	fmt.Println(num)
+}
+
+// 通过通信来共享内存
+
+type Num struct {
+	addChan chan int
+	getChan chan chan int
+}
+
+func NewNum() *Num {
+	addChan := make(chan int)
+	getChan := make(chan chan int)
+
+	go func() {
+		num := 0
+		for {
+			select {
+			case val := <-addChan:
+				num = num + val
+			case respChan := <-getChan:
+				respChan <- num
+			}
+		}
+	}()
+
+	return &Num{
+		addChan: addChan,
+		getChan: getChan,
+	}
+}
+
+func (n *Num) Add(val int) {
+	n.addChan <- val
+}
+
+func (n *Num) Get() int {
+	respChan := make(chan int)
+	n.getChan <- respChan
+	val := <-respChan
+
+	return val
+}
+
+func main() {
+	num := NewNum()
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			num.Add(1)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(num.Get())
 }
