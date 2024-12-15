9510f69b5887abf055ce7e85c2d0c1038682f66b
lang <jingyifsy@gmail.com>
2024-12-15 21:36:19 +0800

    second

--- main.go
+++ main.go
@@ -8,17 +8,19 @@ import (
 // 通过通信来共享内存，而不是通过共享内存来通信
 func main() {
 	num := 0
-
+	var mu sync.Mutex
 	var wg sync.WaitGroup
 
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
-			wg.Done()
+			defer wg.Done()
+			mu.Lock()
 			num++
+			mu.Unlock()
 		}()
 	}
 
-	wg.Done()
+	wg.Wait()
 	fmt.Println(num)
 }
